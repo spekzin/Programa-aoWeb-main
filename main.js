075a01034e90eceda1aceca8f9455eb5c3008e65
spekzin <gabrielallanlopws@gmail.com>
2024-03-19 16:36:49 -0300

    teste

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
-document.getElementById("botaoConfirmar").addEventListener("click", function() {
+document.getElementById("botaoConfirmar").addEventListener("click", function(event) {
+    event.preventDefault(); // Evita o envio do formulário
+
     const corSelecionada = document.getElementById("Cor").value;
     const anoSelecionado = document.getElementById("Ano").value;
     const modeloSelecionado = document.getElementById("Modelo").value;
-    const aviso = document.getElementById("aviso");
 
     let imagens = {
-        "Roxo_2020_Aventador": "jpg/aventadoroxa2020.avif",
-        "Aventador_Preta": "jpg/aventadorpreta.webp",
-        "Aventador_Verde": "jpg/aventadorverde.jpg"      
+        "Roxo_Aventador":    "jpg/aventadoroxa2020.avif",
+        "Verde_Aventador":   "jpg/aventadorverde.jpg",
+        "Preto_Aventador":   "jpg/aventadorpreta.webp",
+        "Branca_Aventador":  "jpg/aventadorbranca.jpg",
+        "Branca_Gallardo":   "jpg/gallardobranca.jpg",
+        "Preta_Gallardo ":   "jpg/gallardopreta.jpg",
+        "Verde_Gallardo":    "jpg/gallardoverde.jpg",
+        "Roxa_Gallardo":     "jpg/gallardoroxa.jpg",
+        "svj_branca":        "jpg/svjbranca.jpg",
+        "svj_preta":         "jpg/svjpreta.jpg",
+        "svj_verde":         "jpg/svjverde.jpg",
+        "svj_roxa":          "jpg/svjroxa.webp",
+        "Urus_Branca":       "jpg/Urusbranca1.jpeg",
+        "Urus_Preta":       "jpg/uruspreta.jpg",
+        "Urus_Verde":       "jpg/urusverde.jpg",
+        "Urus_Roxa":        "jpg/urusroxa.jpg",
     };
 
-    if (corSelecionada === "Cor" || anoSelecionado === "Ano" || modeloSelecionado === "Modelo") {
-        event.preventDefault(); // Evita o envio do formulário
-        aviso.style.display = "block"; // Mostra o aviso
-        
-    } else {
-        let chave = `${corSelecionada}_${anoSelecionado}_${modeloSelecionado}`;
-        if (imagens[chave]) {
-            exibirImagem(imagens[chave]);
-        }
+    let chave = `${corSelecionada}_${anoSelecionado}_${modeloSelecionado}`;
+    if (imagens[chave]) {
+        window.location.href = `paginaFinal.html?imagem=${encodeURIComponent(imagens[chave])}`;
     }
 });
-
-function exibirImagem(urlImagem) {
-    // Cria um elemento img
-    let img = document.createElement('img');
-    // Define o atributo src com a URL da imagem
-    img.src = urlImagem;
-    // Adiciona a imagem ao corpo da página
-    document.body.appendChild(img);
-}
---
+++ paginaFinal.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() { //DOMContentLoaded faz com que a pagina carregue toda pra depois executar algo
+    const params = new URLSearchParams(window.location.search); //pegando os parametros da consulta, cor, ano e modelo
+    const imagem = params.get("imagem"); // Estamos procurando por um parâmetro chamado "imagem" na página. Se existir, estamos guardando essa informação em uma variável chamada "imagem".
+    if (imagem) {
+        exibirImagem(imagem); //se encontrou imagem, exiba.
+    }
+});
+
+function exibirImagem(urlImagem) { 
+    let img = document.createElement('img'); //Cria um novo elemento que sera exibida na pagina
+    img.src = decodeURIComponent(urlImagem); // decodeURIComponent faz com q a imagem seja interpretada corretamente 
+    img.classList.add("imagem-redirecionada");// So serve pra adicionar uma classe pra poder chamar no CSS
+    document.body.appendChild(img) //adiciona a imagem ao corpo no HTML se tirar a imagem n aparece
+};
