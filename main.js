3e986039765455e52cc295ab7996f129d67d0ac4
spekzin <gabrielallanlopws@gmail.com>
2024-03-21 10:53:54 -0300

    ATTNAAULA

--- main.js
+++ main.js
@@ -10,102 +10,27 @@ document.getElementById("botaoConfirmar").addEventListener("click", function(eve
         aviso.style.display = "block"; // Mostra o aviso
     } else {
         let imagens = {
-            "Roxo_2020_Aventador":    "jpg/aventadoroxa2020.avif",
-            "Roxo_2021_Aventador":    "jpg/aventadoroxa2020.avif",
-            "Roxo_2022_Aventador":    "jpg/aventadoroxa2020.avif",
-            "Roxo_2023_Aventador":    "jpg/aventadoroxa2020.avif",
-            "Roxo_2024_Aventador":    "jpg/aventadoroxa2020.avif",
+            "Vermelho_2023_Sedan":    "jpg/aziravermelho.jpg",
+            "Prata_2023_Sedan":    "jpg/aziraprata.jpg",
+            "Branco_2023_Sedan":    "jpg/azirabranco.jpg",
+            "Preto_2023_Sedan":    "jpg/320ipreta.jpg",
             
-            "Verde_2020_Aventador":   "jpg/aventadorverde.jpg",
-            "Verde_2021_Aventador":   "jpg/aventadorverde.jpg",
-            "Verde_2022_Aventador":   "jpg/aventadorverde.jpg",
-            "Verde_2023_Aventador":   "jpg/aventadorverde.jpg",
-            "Verde_2024_Aventador":   "jpg/aventadorverde.jpg",
             
-            "Preto_2020_Aventador":   "jpg/aventadorpreta.webp",
-            "Preto_2021_Aventador":   "jpg/aventadorpreta.webp",
-            "Preto_2022_Aventador":   "jpg/aventadorpreta.webp",
-            "Preto_2023_Aventador":   "jpg/aventadorpreta.webp",
-            "Preto_2024_Aventador":   "jpg/aventadorpreta.webp",
+            "Vermelho_2023_Suv":   "jpg/urusvermelha.jpg",
+            "Prata_2023_Suv":   "jpg/suvprataatt.jpg",
+            "Branco_2023_Suv":   "jpg/urusbrancaatt.jpg",
+            "Preto_2023_Suv":   "jpg/suvpreta.webp",
+          
             
-            "Branco_2020_Aventador":  "jpg/aventadorbranca.jpg",
-            "Branco_2021_Aventador":  "jpg/aventadorbranca.jpg",
-            "Branco_2022_Aventador":  "jpg/aventadorbranca.jpg",
-            "Branco_2023_Aventador":  "jpg/aventadorbranca.jpg",
-            "Branco_2024_Aventador":  "jpg/aventadorbranca.jpg",
+            "Vermelho_2023_Hatch":   "jpg/golfvermelhoatt.jpg",
+            "Prata_2023_Hatch":   "jpg/poloprata.jpg",
+            "Preto_2023_Hatch":   "jpg/golfpretoteste.jpg",
+            "Branco_2023_Hatch":   "jpg/golfbrancoatt.jpg",
             
-            "Branco_2020_Gallardo":   "jpg/gallardobranca.jpg",
-            "Branco_2021_Gallardo":   "jpg/gallardobranca.jpg",
-            "Branco_2022_Gallardo":   "jpg/gallardobranca.jpg",
-            "Branco_2023_Gallardo":   "jpg/gallardobranca.jpg",
-            "Branco_2024_Gallardo":   "jpg/gallardobranca.jpg",
-            
-            "Preto_2020_Gallardo":    "jpg/gallardopreta.jpg",
-            "Preto_2021_Gallardo":    "jpg/gallardopreta.jpg",
-            "Preto_2022_Gallardo":    "jpg/gallardopreta.jpg",
-            "Preto_2023_Gallardo":    "jpg/gallardopreta.jpg",
-            "Preto_2024_Gallardo":    "jpg/gallardopreta.jpg",
-            
-            "Verde_2020_Gallardo":    "jpg/gallardoverde.jpg",
-            "Verde_2021_Gallardo":    "jpg/gallardoverde.jpg",
-            "Verde_2022_Gallardo":    "jpg/gallardoverde.jpg",
-            "Verde_2023_Gallardo":    "jpg/gallardoverde.jpg",
-            "Verde_2024_Gallardo":    "jpg/gallardoverde.jpg",
-
-            "Roxo_2020_Gallardo":     "jpg/gallardoroxa.jpg",
-            "Roxo_2021_Gallardo":     "jpg/gallardoroxa.jpg",
-            "Roxo_2022_Gallardo":     "jpg/gallardoroxa.jpg",
-            "Roxo_2023_Gallardo":     "jpg/gallardoroxa.jpg",
-            "Roxo_2024_Gallardo":     "jpg/gallardoroxa.jpg",
-            
-            "Branco_2020_SVJ":        "jpg/svjbranca.jpg",
-            "Branco_2021_SVJ":        "jpg/svjbranca.jpg",
-            "Branco_2022_SVJ":        "jpg/svjbranca.jpg",
-            "Branco_2023_SVJ":        "jpg/svjbranca.jpg",
-            "Branco_2024_SVJ":        "jpg/svjbranca.jpg",
-
-            "Preto_2020_SVJ":         "jpg/svjpreta.jpg",
-            "Preto_2021_SVJ":         "jpg/svjpreta.jpg",
-            "Preto_2022_SVJ":         "jpg/svjpreta.jpg",
-            "Preto_2023_SVJ":         "jpg/svjpreta.jpg",
-            "Preto_2024_SVJ":         "jpg/svjpreta.jpg",
-
-            "Verde_2020_SVJ":         "jpg/svjverde.jpg",
-            "Verde_2021_SVJ":         "jpg/svjverde.jpg",
-            "Verde_2022_SVJ":         "jpg/svjverde.jpg",
-            "Verde_2023_SVJ":         "jpg/svjverde.jpg",
-            "Verde_2024_SVJ":         "jpg/svjverde.jpg",
-
-            "Roxo_2020_SVJ":          "jpg/svjroxa.webp",
-            "Roxo_2021_SVJ":          "jpg/svjroxa.webp",
-            "Roxo_2022_SVJ":          "jpg/svjroxa.webp",
-            "Roxo_2023_SVJ":          "jpg/svjroxa.webp",
-            "Roxo_2024_SVJ":          "jpg/svjroxa.webp",
-           
-
-            "Branco_2020_Urus":       "jpg/Urusbranca1.jpeg",
-            "Branco_2021_Urus":       "jpg/Urusbranca1.jpeg",
-            "Branco_2022_Urus":       "jpg/Urusbranca1.jpeg",
-            "Branco_2023_Urus":       "jpg/Urusbranca1.jpeg",
-            "Branco_2024_Urus":       "jpg/Urusbranca1.jpeg",
-            
-            "Preto_2020_Urus":        "jpg/uruspreta.jpg",
-            "Preto_2021_Urus":        "jpg/uruspreta.jpg",
-            "Preto_2022_Urus":        "jpg/uruspreta.jpg",
-            "Preto_2023_Urus":        "jpg/uruspreta.jpg",
-            "Preto_2024_Urus":        "jpg/uruspreta.jpg",
-            
-            "Verde_2020_Urus":        "jpg/urusverde.jpg",
-            "Verde_2021_Urus":        "jpg/urusverde.jpg",
-            "Verde_2022_Urus":        "jpg/urusverde.jpg",
-            "Verde_2023_Urus":        "jpg/urusverde.jpg",
-            "Verde_2024_Urus":        "jpg/urusverde.jpg",
-            
-            "Roxo_2020_Urus":         "jpg/urusroxa.jpg",
-            "Roxo_2021_Urus":         "jpg/urusroxa.jpg",
-            "Roxo_2022_Urus":         "jpg/urusroxa.jpg",
-            "Roxo_2023_Urus":         "jpg/urusroxa.jpg",
-            "Roxo_2024_Urus":         "jpg/urusroxa.jpg",
+            "Vermelho_2023_Luxo":  "jpg/ferrarivermelhaatt.jpg",
+            "Prata_2023_Luxo":  "jpg/luxoprata.webp",
+            "Branco_2023_Luxo":  "jpg/ferraribranca.jpg",
+            "Preto_2023_Luxo":  "jpg/ferraripretaatt.jpg",
         };
 
         let chave = `${corSelecionada}_${anoSelecionado}_${modeloSelecionado}`;
