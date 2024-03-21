c6b0a7aaac612c99b54b951edb60bce4849064bd
spekzin <gabrielallanlopws@gmail.com>
2024-03-21 05:12:21 -0300

    correto

--- paginaFinal.js
+++ paginaFinal.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() { //DOMContentLoaded faz com que a pagina carregue toda pra depois executar algo
+document.addEventListener("DOMContentLoaded", function () { //DOMContentLoaded faz com que a pagina carregue toda pra depois executar algo
     const params = new URLSearchParams(window.location.search); //pegando os parametros da consulta, cor, ano e modelo
     const imagem = params.get("imagem"); // Estamos procurando por um parâmetro chamado "imagem" na página. Se existir, estamos guardando essa informação em uma variável chamada "imagem".
     if (imagem) {
@@ -6,9 +6,11 @@ document.addEventListener("DOMContentLoaded", function() { //DOMContentLoaded fa
     }
 });
 
-function exibirImagem(urlImagem) { 
+function exibirImagem(urlImagem) {
     let img = document.createElement('img'); //Cria um novo elemento que sera exibida na pagina
     img.src = decodeURIComponent(urlImagem); // decodeURIComponent faz com q a imagem seja interpretada corretamente 
     img.classList.add("imagem-redirecionada");// So serve pra adicionar uma classe pra poder chamar no CSS
-    document.body.appendChild(img) //adiciona a imagem ao corpo no HTML se tirar a imagem n aparece
-};
+    document.body.appendChild(img);//adiciona a imagem ao corpo no HTML se tirar a imagem n aparece
+}
+
+
